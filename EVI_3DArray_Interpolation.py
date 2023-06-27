fc62fae0991ef6ff32a58ee0b5dc28a4032840e8
DenysUkr <denys.osmak@gmail.com>
2023-06-27 15:14:21 -0700

    Interpelationd Code v1.0

--- EVI_3DArray_Interpolation.py
+++ EVI_3DArray_Interpolation.py
@@ -6,11 +6,11 @@ import imageio
 ROWS = 2272
 COLS = 2485
 
-directory = 'EVI' #source directory
+directory = 'Datasets/EVI' #source directory
 
 currFile = 0
 totalFile = len(os.listdir(directory))
-print("Generating Images...")
+print("Intepelating Imiges...")
 
 #This is a 3D array with this order: [ROWS][COLS][DEPTH]
 InterpelatedArray = [][][]
@@ -47,6 +47,39 @@ for row in range(ROWS)
     for cols in range(COLS)
         InterpelatedArray[row][cols].interpolate(method ='linear', limit_direction ='both')
 
+print("Image Interpelation Compleate... Generating Imiges... ")
+
+currFile = 0
+totalFile = len(totalFile = len(os.listdir(directory))) * 16
 
 
+#Create an image one depth at a time
+for depth in range(InterpelatedArray)
 
+    print("File: " + str(currFile) + "/" + str(totalFile))
+
+    #create blank image
+    im = Image.new(mode="RGB", size=(ROWS, COLS))
+    pixels = im.load()
+
+    #open EVI tif file
+    EVI = Image.open(f)
+    EVIarray = np.array(EVI)
+
+
+    #loop through pixels
+    for r in range(ROWS):
+        for c in range(COLS):
+            #if EVI is valid, fill value of blank array
+            if(InterpelatedArray[row][ccol][depth] > 0):
+                value = int(((EVIarray[row][col][depth]+2000)/12000)*255)
+                pixels[r,c] = (value,value,value)
+            #if invalid, set to white
+            else:
+                pixels[r,c] = (255,255,255)
+    print(f)
+    #transpose image
+    im = im.transpose(Image.ROTATE_270)
+    im = im.transpose(Image.FLIP_LEFT_RIGHT)
+    #write file
+    imageio.imwrite("C:\Users\denys\Documents\GitHub\Project_2\Datasets\EVI_Interpelated" + ".png", im)     #removed + f[:4]
