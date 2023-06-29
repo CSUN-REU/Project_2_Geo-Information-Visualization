f10ebca1687573cfeabb254ec9b160046214801b
DenysUkr <denys.osmak@gmail.com>
2023-06-29 11:57:56 -0700

    Inperpelation v1.0.1

--- EVI_3DArray_Interpolation.py
+++ EVI_3DArray_Interpolation.py
@@ -5,44 +5,60 @@ import imageio
 
 ROWS = 2272
 COLS = 2485
+TotalFilesTemp = 320
+
 
 directory = 'Datasets/EVI' #source directory
 
 currFile = 0
-totalFile = len(os.listdir(directory))
-print("Intepelating Imiges...")
+totalFile = len(os.listdir(directory))*16
+
+print("Generating the Array...")
 
 #This is a 3D array with this order: [ROWS][COLS][DEPTH]
-InterpelatedArray = numpy.zeros((m,n,p))
+InterpelatedArray = np.empty((ROWS, COLS,TotalFilesTemp))
 depth = 0   #Starting depth
 
+FileArray = np.zeros(len(os.listdir(directory)))    
 
 #iterate over files and copy them into the 3D array
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
-
-        print("File: " + str(currFile) + "/" + str(totalFile))
-
+        print("TIIF Stored: " + str(currFile) + "/" + '231')
         #open EVI tif file
         EVI = Image.open(f)
-        EVIarray = np.array(EVI)
+        FileArray[filename] = EVI
 
-        #loop through pixels of the image
-        for row in range(ROWS):
-            for ccol in range(COLS):
+#Looping though each of the EVI 
+for EVI_File in range len(FileArray):
+    #loop through pixels of the image
+    for row in range(ROWS):
 
-                #traverse the rows and colloms and copy them into the new array
-                InterpelatedArray[row][cols][depth] = EVIarray[row][cols]
+        #Making sure that I don't go outside the bounds of the index
+        if row+1 < len(FileArray)-1:
+            EVI1 = FileArray[row]
+            EVI2 = FileArray[row+1] 
 
-            #used to traverse the EVI files
-            currFile += 1
+        for cols in range(COLS):
 
-            #used to traverse the 3D array's depth
-            depth += 16
+            #traverse the rows and colloms and copy them into the new array
+            InterpelatedArray[row][cols][depth] = EVIarray[row][cols]
             
 
-#Iterate by each pixel and inteperating the DEPTH array
+
+#this is used to append 15 empty arrays between the avalible EVI imiges
+for tempDepth in range(15):
+    #used to traverse the 3D array's depth
+    if(depth < TotalFilesTemp-1):
+        tempEmptyArray = np.empty((ROWS, COLS, 1, like=InterpelatedArray)
+        np.append(InterpelatedArray, tempEmptyArray, axis=2)
+        depth += 1
+
+            
+print("Interpelating Iniges...")
+
+#Iterpelate by each pixel and by the DEPTH array
 for row in range(ROWS):
     for cols in range(COLS):
         InterpelatedArray[row][cols].interpolate(method ='linear', limit_direction ='both')
@@ -55,7 +71,8 @@ totalFile = len(totalFile = len(os.listdir(directory))) * 16
 
 #Create an image one depth at a time
 for depth in range(InterpelatedArray):
-
+    
+    print(currFile)
     print("File: " + str(currFile) + "/" + str(totalFile))
 
     #create blank image
@@ -68,18 +85,18 @@ for depth in range(InterpelatedArray):
 
 
     #loop through pixels
-    for r in range(ROWS):
-        for c in range(COLS):
+    for row in range(ROWS):
+        for col in range(COLS):
             #if EVI is valid, fill value of blank array
-            if(InterpelatedArray[row][ccol][depth] > 0):
+            if(InterpelatedArray[row][col][depth] > 0):
                 value = int(((EVIarray[row][col][depth]+2000)/12000)*255)
-                pixels[r,c] = (value,value,value)
+                pixels[row,col] = (value,value,value)
             #if invalid, set to white
             else:
-                pixels[r,c] = (255,255,255)
+                pixels[row,col] = (255,255,255)
     print(f)
     #transpose image
     im = im.transpose(Image.ROTATE_270)
     im = im.transpose(Image.FLIP_LEFT_RIGHT)
     #write file
-    imageio.imwrite("C:\Users\denys\Documents\GitHub\Project_2\Datasets\EVI_Interpelated" + ".png", im)     #removed + f[:4]
+    imageio.imwrite(r"C:\Users\denys\Documents\GitHub\Project_2\Datasets\EVI_Interpelated.png") #removed + f[:4]
