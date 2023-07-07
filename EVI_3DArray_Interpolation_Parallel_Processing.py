8fc1781f5831f86b6ff16ebb1b4b6de58fef308b
DenysUkr <denys.osmak@gmail.com>
2023-07-07 09:45:51 -0700

    Interpelation Updated Final

--- EVI_3DArray_Interpolation_Parallel_Processing.py
+++ EVI_3DArray_Interpolation_Parallel_Processing.py
@@ -46,23 +46,18 @@ def interpalateImiges(parameterArray):
             #filling in the interpelation data for one pixel for the 14 days in the 3D array
             for x in range(len(InterpelatedArray)): 
                 #I am multipling the data by 1000 to scale it and not loose the data
-                InterpelatedArray[x][row][cols]= int((y1+((x+2)-1)*((y2-y1)/(17-1))) *1000)  
-                
-                #print(str(InterpelatedArray[x][row][cols]))
-                #print("Pixel Row: " + str(row) + " Pixel Column: " + str(cols) + " Pixel #: " + str(x) + " / 16")   
+                InterpelatedArray[x][row][cols]= int((y1+((x+2)-1)*((y2-y1)/(17-1))) *1000)     
 
-    #print(InterpelatedArray[EVI_File])
-
-    #print("Image Interpelation Compleate: " + str(itteration) + " / " + str(dirFile-1) + " ...Generating Imiges... ")
-    #itteration += 1
 
     #Generating Tiff Files Using NpArray 
     pathCounter = 1
     for y in range(15):
-        #InterpelatedArray = InterpelatedArray.astype(np.uint16)
         Data = Image.fromarray(InterpelatedArray[y])
         FilePathName = parameterArray[0]
-        intigerPath = str(int(FilePathName[46:53]) + pathCounter)
+        IntigerIndex = FilePathName.index("doy") + 3
+        print(str(IntigerIndex))
+        intigerPath = str(int(FilePathName[IntigerIndex:IntigerIndex+7]) + pathCounter)
+        print(intigerPath)
         pathCounter += 1
         saveName =  "MOD13A1.061__500m_16_days_EVI_doy" + str(intigerPath) + str(parameterArray[53:])
         saveLocation = "C:/Users/denys/Documents/GitHub/Project_2/Datasets/EVI_Interpelated/" + str(saveName) + ".tif"
@@ -80,7 +75,6 @@ if __name__ == '__main__':
         f = os.path.join(directory, filename)
         if os.path.isfile(f):
             FileArray[count] = f
-            #print(FileArray[count])
             print("TIIF Stored: " + str(currFile) + "/" + str(dirFile))
             currFile += 1
         count += 1
@@ -95,15 +89,11 @@ if __name__ == '__main__':
         EVI1 = FileArray[i]
         EVI2 = FileArray[i+1]
         parameterArray[i] = (EVI1, EVI2)
-        #print(EVI1[46:53])
         print(str(int(EVI1[46:53])+1))
         print(EVI1[53:])
         
-    #print(parameterArray[0][0])
-    #print(parameterArray[0][1])
     
     print("Generating Images...")
-    #interpalateImiges(parameterArray[0])
 
     pool = Pool()                   # Create a multiprocessing Pool
     pool.map(interpalateImiges, parameterArray)  # process files iterable with pool
